aab7cf76346a2f2ac4c316e47e4880215d82cd04
heimixyz <$ ssh-keygen -t rsa -b 4096 -C "your_email@example.com>
2020-05-28 13:10:15 +0800

    Create bbs.rar

---
+++ bbs/bbs/log/202005/admin_login_error.php
@@ -0,0 +1,4 @@
+<?php exit;?>	2020-05-10 14:59:47	192.168.31.12	/admin/?index-login.htm	1	password error. uid:1 - ******f8427e
+<?php exit;?>	2020-05-10 14:59:48	192.168.31.12	/admin/?index-login.htm	1	password error. uid:1 - ******f8427e
+<?php exit;?>	2020-05-10 14:59:48	192.168.31.12	/admin/?index-login.htm	1	password error. uid:1 - ******f8427e
+<?php exit;?>	2020-05-10 14:59:48	192.168.31.12	/admin/?index-login.htm	1	password error. uid:1 - ******f8427e
